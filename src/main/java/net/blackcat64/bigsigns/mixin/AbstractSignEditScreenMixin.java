2c99f05ba846fdcba9b2aef441c5a5386ca2d5ac
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-11-18 11:02:20 +0000

    Fixed One-Line Sign GUI. It now restricts editing to only 1 line.

--- src/main/java/net/blackcat64/bigsigns/mixin/AbstractSignEditScreenMixin.java
+++ src/main/java/net/blackcat64/bigsigns/mixin/AbstractSignEditScreenMixin.java
@@ -1,9 +1,11 @@
 package net.blackcat64.bigsigns.mixin;
 
 import net.blackcat64.bigsigns.block.entity.OneLineSignBlockEntity;
+import net.minecraft.client.gui.font.TextFieldHelper;
 import net.minecraft.client.gui.screens.inventory.AbstractSignEditScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.block.entity.SignBlockEntity;
+import org.checkerframework.checker.units.qual.A;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -24,11 +26,21 @@ public class AbstractSignEditScreenMixin {
             this.line = 1;
     }
 
+    @Inject(method = "init", at = @At("HEAD"))
+    private void injectInit(CallbackInfo info) {
+        if (sign instanceof OneLineSignBlockEntity)
+            this.line = 1;
+    }
+
     @Inject(method = "keyPressed", at = @At("HEAD"), cancellable = true)
     private void injectOnKeyPress(int pKeyCode, int pScanCode, int pModifiers, CallbackInfoReturnable<Boolean> info) {
         if (sign instanceof OneLineSignBlockEntity) {
             this.line = 1;
+            if (pKeyCode != 265 && pKeyCode != 264 && pKeyCode != 257 && pKeyCode != 335) // If the key pressed is NOT Up arrow, down arrow or Enter, handle it normally
+                return;
+
             info.setReturnValue(true);
+            info.cancel();
         }
     }
 
