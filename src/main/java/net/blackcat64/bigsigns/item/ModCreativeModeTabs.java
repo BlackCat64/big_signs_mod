24acde47c974b5c029f8ce84f29b3e934c02f81b
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-12-20 15:08:51 +0000

    Added Gray Metal Hanging Sign to classes.

--- src/main/java/net/blackcat64/bigsigns/item/ModCreativeModeTabs.java
+++ src/main/java/net/blackcat64/bigsigns/item/ModCreativeModeTabs.java
@@ -32,6 +32,9 @@ public class ModCreativeModeTabs {
                         pOutput.accept(ModItems.ONE_LINE_CRIMSON_SIGN.get());
                         pOutput.accept(ModItems.ONE_LINE_WARPED_SIGN.get());
 
+                        pOutput.accept(ModItems.ONE_LINE_BLACK_METAL_SIGN.get());
+                        pOutput.accept(ModItems.ONE_LINE_GRAY_METAL_SIGN.get());
+
                         // add one-line hanging sign items
                         pOutput.accept(ModItems.ONE_LINE_OAK_HANGING_SIGN.get());
                         pOutput.accept(ModItems.ONE_LINE_SPRUCE_HANGING_SIGN.get());
@@ -45,8 +48,8 @@ public class ModCreativeModeTabs {
                         pOutput.accept(ModItems.ONE_LINE_CRIMSON_HANGING_SIGN.get());
                         pOutput.accept(ModItems.ONE_LINE_WARPED_HANGING_SIGN.get());
 
-                        pOutput.accept(ModItems.ONE_LINE_BLACK_METAL_SIGN.get());
                         pOutput.accept(ModItems.ONE_LINE_BLACK_METAL_HANGING_SIGN.get());
+                        pOutput.accept(ModItems.ONE_LINE_GRAY_METAL_HANGING_SIGN.get());
                     })
                     .build());
 
