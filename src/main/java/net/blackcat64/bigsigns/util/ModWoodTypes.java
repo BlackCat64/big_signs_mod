349a81e3134ce1c9c2f3853435fc234791a0985d
BlackCat64 <150248561+BlackCat64@users.noreply.github.com>
2024-12-22 19:08:30 +0000

    Added all required wood types for the metallic signs.

--- src/main/java/net/blackcat64/bigsigns/util/ModWoodTypes.java
+++ src/main/java/net/blackcat64/bigsigns/util/ModWoodTypes.java
@@ -6,5 +6,24 @@ import net.minecraft.world.level.block.state.properties.WoodType;
 
 public class ModWoodTypes {
     public static final WoodType BLACK_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":black_metal_sign", BlockSetType.IRON));
+    public static final WoodType BLUE_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":blue_metal_sign", BlockSetType.IRON));
+    public static final WoodType BROWN_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":brown_metal_sign", BlockSetType.IRON));
+    public static final WoodType CREAM_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":cream_metal_sign", BlockSetType.IRON));
+    public static final WoodType CYAN_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":cyan_metal_sign", BlockSetType.IRON));
+    public static final WoodType DARK_ORANGE_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":dark_orange_metal_sign", BlockSetType.IRON));
+    public static final WoodType DARK_PINK_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":dark_pink_metal_sign", BlockSetType.IRON));
+    public static final WoodType DARK_RED_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":dark_red_metal_sign", BlockSetType.IRON));
     public static final WoodType GRAY_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":gray_metal_sign", BlockSetType.IRON));
+    public static final WoodType GREEN_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":green_metal_sign", BlockSetType.IRON));
+    public static final WoodType LIGHT_BLUE_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":light_blue_metal_sign", BlockSetType.IRON));
+    public static final WoodType LIGHT_GRAY_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":light_gray_metal_sign", BlockSetType.IRON));
+    public static final WoodType LIME_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":lime_metal_sign", BlockSetType.IRON));
+    public static final WoodType MAGENTA_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":magenta_metal_sign", BlockSetType.IRON));
+    public static final WoodType ORANGE_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":orange_metal_sign", BlockSetType.IRON));
+    public static final WoodType PINK_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":pink_metal_sign", BlockSetType.IRON));
+    public static final WoodType PURPLE_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":purple_metal_sign", BlockSetType.IRON));
+    public static final WoodType RED_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":red_metal_sign", BlockSetType.IRON));
+    public static final WoodType WHITE_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":white_metal_sign", BlockSetType.IRON));
+    public static final WoodType YELLOW_METAL_SIGN = WoodType.register(new WoodType(BigSignsMod.MOD_ID + ":yellow_metal_sign", BlockSetType.IRON));
 }
+
